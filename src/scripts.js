b1c1783c41c773fbc3a33d39b337ce24aaa8eb46
Anna Johnson <ae.johnson2931@gmail.com>
2021-09-21 11:54:07 -0700

    Add class frameworks and export/imports

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,17 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
+//IMPORTS///////////////////////
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import { fetchCustomers, fetchRooms, fetchBookings } from './apiCalls.js';
+import Customer from './classes/customer.js';
+import Room from './classes/room.js';
+import Booking from './classes/booking.js';
 
+//GLOBAL VARIABLE//////////////////
 
-console.log('This is the JavaScript entry file - your code begins here.');
+//FETCH CALLS//////////////////////
+
+//EVENT LISTENERS//////////////////
+
+//FUNCTIONS
